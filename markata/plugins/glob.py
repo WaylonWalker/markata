3bf940da26d67a5447ff48b3f7b4400b46416a41
Waylon Walker <walker_waylon_s@cat.com>
2021-03-21 09:08:44 -0500

    fix: mypy type errors

--- markata/plugins/glob.py
+++ markata/plugins/glob.py
@@ -5,9 +5,14 @@ from more_itertools import flatten
 
 from markata.hookspec import hook_impl
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from markata import Markata
+
 
 @hook_impl
-def glob(markata):
-    markata.files = flatten(
-        [Path().glob(pattern) for pattern in markata.config["glob_patterns"]]
-    )
+def glob(markata: "Markata") -> None:
+    patterns = list(markata.config["glob_patterns"])
+    markata.files = list(flatten([Path().glob(str(pattern)) for pattern in patterns]))
+    markata.content_directories = list(set([f.parent for f in markata.files]))
