606e33fcae3f5b487e386ba9e762aeca2aa6e7d7
Waylon Walker <walker_waylon_s@cat.com>
2021-03-22 19:56:04 -0500

    type checking post_template

--- markata/plugins/post_template.py
+++ markata/plugins/post_template.py
@@ -2,10 +2,15 @@ from jinja2 import Template
 
 from markata.hookspec import hook_impl
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from markata import Markata
+
 
 @hook_impl
-def render(markata):
-    template_file = markata.config["post_template"]
+def render(markata: "Markata") -> None:
+    template_file = markata.post_template
     with open(template_file) as f:
         template = Template(f.read())
     for article in markata.iter_articles("apply template"):
@@ -14,6 +19,6 @@ def render(markata):
             body=article.html,
             title=article.metadata["title"],
             slug=article.metadata["slug"],
-            toc=markata.md.toc,
+            toc=markata.md.toc,  # type: ignore
             date=article.metadata["date"],
         )
