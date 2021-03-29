6330c79b92c85b19256fc39db3b19032b82eeb4a
Waylon Walker <walker_waylon_s@cat.com>
2021-03-28 21:53:26 -0500

    create describe module

---
+++ markata/cli/describe.py
@@ -0,0 +1,53 @@
+from typing import TYPE_CHECKING
+
+from rich.panel import Panel
+from rich.table import Table
+
+if TYPE_CHECKING:
+    from markata import Markata
+
+
+class Describe:
+    def __init__(self, m: "Markata", simple: bool = False) -> None:
+        self.m = m
+        self.simple = simple
+
+    def __rich__(self) -> Panel:
+        grid = Table.grid(expand=True)
+        # grid.add_row(f"{len(self.m.articles)} articles")
+        grid.add_row(f"[bright_blue]{len(self.m.articles)}[/] articles")
+        grid.add_row(
+            f"[green]{len([a for a in self.m.articles if a['status'] =='published'])}[/] published"
+        )
+        grid.add_row(
+            f"[gold1]{len([a for a in self.m.articles if a['status'] =='draft'])}[/] drafts"
+        )
+        grid.add_row("")
+        grid.add_row("[bold gold1]TAGS[/]")
+        from collections import Counter
+
+        from more_itertools import flatten
+
+        for tag, count in (
+            Counter(list(flatten([a["tags"] for a in self.m.articles]))).most_common()
+            # .items()
+        ):
+            grid.add_row(f'{count} {" "*(3-len(str(count)))} {tag}')
+        if self.simple:
+            return grid
+        else:
+            return Panel(grid, title="[gold1]describe[/]", border_style="magenta")
+
+
+if __name__ == "__main__":
+    from rich import print
+
+    # from rich.live import Live
+    from markata import Markata
+
+    # from .cli import run_until_keyboard_interrupt
+
+    m = Markata()
+    print(Describe(m, simple=True))
+    # with Live(Describe(m), refresh_per_second=1, screen=True):
+    #     run_until_keyboard_interrupt()
