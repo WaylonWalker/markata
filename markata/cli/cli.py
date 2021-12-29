46e5a00b7f8287ecf32481511b67946054b534f5
Waylon S. Walker <waylon@waylonwalker.com>
2021-12-28 22:53:14 -0600

    create typer app

--- markata/cli/cli.py
+++ markata/cli/cli.py
@@ -1,8 +1,9 @@
 import time
+import typer
 
 from rich.layout import Layout
 
-from markata import Markata
+from markata import Markata, __version__
 
 
 class MarkataCli(Markata):
@@ -18,11 +19,10 @@ def make_layout() -> Layout:
         Layout(name="header", size=3),
         Layout(name="main"),
     )
-    layout["main"].split(
-        Layout(name="side", ratio=0.5, minimum_size=30),
-        Layout(name="mid"),
-        Layout(name="describe", ratio=0.2, minimum_size=30),
-        direction="horizontal",
+    layout["main"].split_row(
+        Layout(name="side", ratio=50),
+        Layout(name="mid", ratio=30),
+        Layout(name="describe", ratio=20),
     )
     layout["mid"].split(
         Layout(name="server"),
@@ -37,6 +37,11 @@ def make_layout() -> Layout:
 class RichM:
     def __init__(self, markata: MarkataCli):
         self.m = markata
+        self.layout = make_layout()
+
+    def __rich__(self):
+
+        return self.layout
 
 
 def run_until_keyboard_interrupt():
@@ -45,3 +50,36 @@ def run_until_keyboard_interrupt():
             time.sleep(0.2)
     except KeyboardInterrupt:
         pass
+
+
+def version_callback(value: bool):
+    if value:
+        typer.echo(f"Markata CLI Version: {__version__}")
+        raise typer.Exit()
+
+
+app = typer.Typer(
+    name="Markata",
+    help="Awesome CLI user manager.",
+)
+
+
+@app.callback()
+def main(
+    version: bool = typer.Option(
+        None, "--version", callback=version_callback, is_eager=True
+    ),
+):
+    # Do other global stuff, handle other global options here
+    return
+
+
+def cli():
+    m = Markata()
+    m.configure()
+    m._pm.hook.cli(markata=m, app=app)
+    app()
+
+
+if __name__ == "__main__":
+    cli()
