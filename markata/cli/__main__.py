1775b16034e4c79c75d96fdf53cdb585ca173474
Waylon S. Walker <waylon@waylonwalker.com>
2022-01-08 11:21:10 -0600

    convert to textual

--- markata/cli/__main__.py
+++ markata/cli/__main__.py
@@ -1,4 +1,7 @@
 from rich.live import Live
+from markata import Markata
+from textual.widget import Widget
+from rich.console import RenderableType
 
 from . import (
     Describe,
@@ -12,24 +15,50 @@ from . import (
 )
 
 
-def run() -> None:
-    layout = make_layout()
-    layout["header"].update(Header())
+from textual.app import App
 
-    m = MarkataCli()
-    server = Server(directory=m.output_dir)
-    layout["server"].update(server)
 
-    layout["plugins"].update(Plugins(m))
-    m.count = 0
-    m._dirhash = ""
-    m.load()
-    layout["runner"].update(Runner())
-    layout["describe"].update(Describe(m))
+class MarkataWidget(Widget):
+    def __init__(self, markata: Markata, widget: str = "server"):
+        super().__init__(widget)
+        self.m = markata
+        self.widget = widget
+        self.renderable = getattr(self.m, self.widget)
 
-    with Live(layout, refresh_per_second=1, screen=True, console=m.console):
-        run_until_keyboard_interrupt()
+    def render(self):
+        return self.renderable
+
+    async def update(self, renderable: RenderableType) -> None:
+        self.renderable = renderable
+        self.refresh()
+
+
+class MarkataApp(App):
+    async def on_mount(self) -> None:
+        self.m = Markata()
+        self.server = MarkataWidget(self.m, "server")
+        self.runner = MarkataWidget(self.m, "runner")
+        self.plugins = MarkataWidget(self.m, "plugins")
+        self.summary = MarkataWidget(self.m, "summary")
+        await self.view.dock(self.plugins, edge="left", size=30, name="plugins")
+        await self.view.dock(self.summary, edge="right", size=30, name="summary")
+        await self.view.dock(self.server, self.runner, edge="top")
+        self.set_interval(1, self.action_refresh)
+
+    async def on_load(self, event):
+        await self.bind("q", "my_quit")
+        await self.bind("r", "refresh")
+
+    async def action_my_quit(self) -> None:
+        await self.action_quit()
+
+    async def action_refresh(self) -> None:
+        self.refresh()
+        self.runner.refresh()
+        self.server.refresh()
+        self.plugins.refresh()
+        self.summary.refresh()
 
 
 if __name__ == "__main__":
-    run()
+    MarkataApp.run(log="textual.log")
