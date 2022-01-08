6e212e8fc0e85b5d16b3f76deaee645382f4610a
Waylon S. Walker <waylon@waylonwalker.com>
2022-01-08 17:42:47 -0600

    implement total_ordering

--- markata/lifecycle.py
+++ markata/lifecycle.py
@@ -1,19 +1,17 @@
 from enum import Enum, auto
+from functools import total_ordering
 
 
+@total_ordering
 class LifeCycle(Enum):
+
     configure = auto()
     glob = auto()
+    load = auto()
     pre_render = auto()
     render = auto()
     post_render = auto()
     save = auto()
-
-    def __gt__(self, other):
-        try:
-            return self.value > other.value
-        except AttributeError:
-            return self.value > other
 
     def __lt__(self, other):
         try:
