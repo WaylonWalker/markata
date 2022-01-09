c0f053e9c085ce75ae0c4bb170c474c49a53b6b4
Waylon S. Walker <waylon@waylonwalker.com>
2022-01-08 18:13:19 -0600

    get output_dir from config

--- markata/plugins/to_json.py
+++ markata/plugins/to_json.py
@@ -10,6 +10,6 @@ if TYPE_CHECKING:
 
 @hook_impl
 def save(markata: "Markata") -> None:
-    output_file = Path(markata.output_dir) / "markata.json"
+    output_file = Path(markata.config["output_dir"]) / "markata.json"
     output_file.parent.mkdir(parents=True, exist_ok=True)
     output_file.write_text(json.dumps(markata.to_dict(), default=str))
