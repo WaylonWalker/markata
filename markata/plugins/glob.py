2e00d3b8d3861f06baa0000449227b1f3ecb9eab
Waylon S. Walker <waylon@waylonwalker.com>
2022-01-08 19:30:01 -0600

    respect .gitignore while globbing

--- markata/plugins/glob.py
+++ markata/plugins/glob.py
@@ -13,7 +13,30 @@ if TYPE_CHECKING:
 @hook_impl
 @register_attr("content_directories", "files")
 def glob(markata: "Markata") -> None:
+
     markata.files = list(
         flatten([Path().glob(str(pattern)) for pattern in markata.glob_patterns])
     )
     markata.content_directories = list(set([f.parent for f in markata.files]))
+
+    try:
+        ignore = markata.config["glob"]["use_gitignore"] or True
+    except KeyError:
+        ignore = True
+
+    if ignore and (Path(".gitignore").exists() or Path(".markataignore").exists()):
+        import pathspec
+
+        lines = []
+
+        if Path(".gitignore").exists():
+            lines.extend(Path(".gitignore").read_text().splitlines())
+
+        if Path(".markataignore").exists():
+            lines.extend(Path(".markataignore").read_text().splitlines())
+
+        spec = pathspec.PathSpec.from_lines("gitwildmatch", lines)
+
+        markata.files = [
+            file for file in markata.files if not spec.match_file(str(file))
+        ]
