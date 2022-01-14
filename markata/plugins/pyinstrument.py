23f173ca9db77c1bda6d50b7c2d906ab47ad6ebb
Waylon S. Walker <waylon@waylonwalker.com>
2022-01-13 19:26:20 -0600

    add should_profile

--- markata/plugins/pyinstrument.py
+++ markata/plugins/pyinstrument.py
@@ -4,9 +4,10 @@ Markata plugin to create a pyinstrument profile if pyinstrument is installed.
 The profile will be saved to <output_dir>/_profile/index.html
 """
 from pathlib import Path
+import typer
 from typing import TYPE_CHECKING
 
-from markata.hookspec import hook_impl
+from markata.hookspec import hook_impl, register_attr
 
 if TYPE_CHECKING:
     from markata import Markata
@@ -19,14 +20,47 @@ except ModuleNotFoundError:
 
 
 @hook_impl(tryfirst=True)
+def cli(app, markata: "Markata") -> None:
+    def should_profile_callback(value: bool):
+        if value:
+            markata.should_profile = True
+
+    @app.callback()
+    def should_profile(
+        version: bool = typer.Option(
+            None, "--profile", callback=should_profile_callback, is_eager=True
+        ),
+    ):
+        # Do other global stuff, handle other global options here
+        return
+
+    @app.command()
+    def profile():
+        ...
+
+
+@hook_impl(tryfirst=True)
+@register_attr("should_profile")
+def configure(markata: "Markata") -> None:
+    "set the should_profile variable"
+
+    try:
+        markata.should_profile = markata.config["pyinstrument"]["should_profile"]
+    except KeyError:
+        markata.should_profile = False
+
+
+@hook_impl(tryfirst=True)
+@register_attr("profiler")
 def glob(markata: "Markata") -> None:
     "start the profiler as soon as possible"
-    try:
-        markata.profiler = Profiler()
-        markata.profiler.start()
-    except NameError:
-        "ignore if Profiler does not exist"
-        ...
+    if markata.should_profile:
+        try:
+            markata.profiler = Profiler()
+            markata.profiler.start()
+        except NameError:
+            "ignore if Profiler does not exist"
+            ...
 
 
 @hook_impl(trylast=True)
