414cab83ac7026ab7f29c641892491fcc6b0c61c
Waylon S. Walker <waylon@waylonwalker.com>
2022-01-15 12:36:33 -0600

    respect index files

--- markata/plugins/publish_html.py
+++ markata/plugins/publish_html.py
@@ -9,10 +9,14 @@ if TYPE_CHECKING:
 
 @hook_impl
 def save(markata: "Markata") -> None:
-    output_dir = Path(markata.config["output_dir"])
+    output_dir = Path(markata.config["output_dir"])  # type: ignore
     output_dir.mkdir(parents=True, exist_ok=True)
+
     for article in markata.articles:
-        article_path = output_dir / article["slug"] / "index.html"
+        if article["slug"] == "index":
+            article_path = output_dir / "index.html"
+        else:
+            article_path = output_dir / article["slug"] / "index.html"
         article_path.parent.mkdir(parents=True, exist_ok=True)
         with open(article_path, "w+") as f:
             f.write(article.html)
