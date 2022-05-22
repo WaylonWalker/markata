5d4eeee848c9ff07dad6bdd2884a2c5ae2810eec
Waylon S. Walker <waylon@waylonwalker.com>
2022-05-22 11:42:46 -0500

    allow explicitly set output_html

--- markata/plugins/publish_html.py
+++ markata/plugins/publish_html.py
@@ -60,15 +60,41 @@ if TYPE_CHECKING:
 
 
 @hook_impl
-def save(markata: "Markata") -> None:
+def pre_render(markata: "Markata") -> None:
+    """
+    Sets the `output_html` in the articles metadata.  If the output is
+    explicitly given, it will make sure its in the `output_dir`, if it is not
+    explicitly set it will use the articles slug.
+    """
     output_dir = Path(markata.config["output_dir"])  # type: ignore
     output_dir.mkdir(parents=True, exist_ok=True)
 
     for article in markata.articles:
-        if article["slug"] == "index":
-            article_path = output_dir / "index.html"
+        if "output_html" in article.metadata:
+            if not Path(article.get("output_html")).is_relative_to(output_dir):
+                article["output_html"] = output_dir / article["output_html"]
+        elif article["slug"] == "index":
+            article["output_html"] = output_dir / "index.html"
         else:
-            article_path = output_dir / article["slug"] / "index.html"
-        article_path.parent.mkdir(parents=True, exist_ok=True)
-        with open(article_path, "w+") as f:
-            f.write(article.html)
+            article["output_html"] = output_dir / article["slug"] / "index.html"
+
+
+@hook_impl
+def save(markata: "Markata") -> None:
+    """
+    Saves all the articles to their set `output_html` location if that location
+    is relative to the specified `output_dir`.  If its not relative to the
+    `output_dir` it will log an error and move on.
+    """
+    output_dir = Path(markata.config["output_dir"])  # type: ignore
+
+    for article in markata.articles:
+        article_path = Path(article["output_html"])
+        if Path(article_path).is_relative_to(output_dir):
+            article_path.parent.mkdir(parents=True, exist_ok=True)
+            with open(article_path, "w+") as f:
+                f.write(article.html)
+        else:
+            markata.console.log(
+                f'article "{article["path"]}" attempted to write to "{article["output_html"]}" outside of the configured output_dir "{output_dir}"'
+            )
