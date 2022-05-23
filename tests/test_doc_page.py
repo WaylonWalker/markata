3bc3b6657f0f72fc61a6a6423fd6cb531928509b
Waylon S. Walker <waylon@waylonwalker.com>
2022-05-22 19:37:03 -0500

    doc pages writes to it's directory, not index

--- tests/test_doc_page.py
+++ tests/test_doc_page.py
@@ -44,7 +44,6 @@ def make_project(tmp_path_factory: Any) -> Any:
                 "markata.plugins.docs",
                 "default",
                 ]
-                
             """
         )
     )
@@ -73,7 +72,7 @@ def test_markout_exists(test_run: Any) -> Any:
 
 def test_index_exists(test_run: Any) -> Any:
     markout = test_run / "markout"
-    index = markout / "index.html"
+    index = markout / "my_module" / "index.html"
     assert index.exists()
 
 
