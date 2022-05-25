ea45f5e0baee10000f70138a78d4ceb0eb1ee2ce
Waylon S. Walker <waylon@waylonwalker.com>
2022-05-25 09:09:58 -0500

    wip logging

--- markata/plugins/setup_logging.py
+++ markata/plugins/setup_logging.py
@@ -1,3 +1,57 @@
+"""
+Setup Logging hook sets up the RichHandler for pretty console logs, and file logs to the configured markata's configured `log_dir`, or
+`output_dir/_logs` if `log_dir` is not configured.  The log file will be named after the `<levelname>.log`
+
+## The log files
+
+There will be 6 log files created based on log level and file type.
+
+```
+markout/_logs
+├── debug
+│   └── index.html
+├── debug.log
+├── info
+│   └── index.html
+├── info.log
+├── warning
+│   └── index.html
+└── warning.log
+```
+
+## Configuration
+
+Ensure that setup_logging is in your hooks.  You can check if `setup_logging`
+is in your hooks by running `markata list --hooks` from your terminal and
+checking the output, or creating an instance of `Markata()` and checking the
+`Markata().hooks` attribute.  If its missing or you wan to be more explicit,
+you can add `setup_logging` to your `markata.toml` `[markata.hooks]`.
+
+``` toml
+[markata]
+
+# make sure its in your list of hooks
+hooks=[
+   "markata.plugins.setup_logging",
+   ]
+```
+
+
+## Disable Logging
+
+If you do not want logging, you can explicityly disable it by adding it to your
+`[markata.disabled_hooks]` array in your `[markata.toml]`
+
+``` toml
+[markata]
+
+# make sure its in your list of hooks
+disabled_hooks=[
+   "markata.plugins.setup_logging",
+   ]
+```
+
+"""
 import datetime
 import logging
 import sys
@@ -18,7 +72,67 @@ class SilentUndefined(Undefined):
         return ""
 
 
+def has_rich_handler() -> bool:
+    """
+    Returns a boolean whether or not there is a RichHandler attached to the
+    root logger.
+    """
+
+    logger = logging.getLogger()
+    return bool([h for h in logger.handlers if isinstance(h, RichHandler)])
+
+
+def has_file_handler(log_file: Path) -> bool:
+    logger = logging.getLogger()
+    existing_logger_files = [
+        l.baseFilename for l in logger.handlers if isinstance(l, logging.FileHandler)
+    ]
+    return str(log_file.absolute()) in existing_logger_files
+
+
 def setup_log(markata: "Markata", level: int = logging.INFO) -> Path:
+    setup_html_log(markata, level)
+    setup_text_log(markata, level)
+
+
+def setup_text_log(markata: "Markata", level: int = logging.INFO) -> Path:
+    """
+    sets up a plain text log in markata's configured `log_dir`, or
+    `output_dir/_logs` if `log_dir` is not configured.  The log file will be
+    named after the `<levelname>.log`
+    """
+    log_file = Path(
+        str(
+            markata.config.get(
+                "log_dir",
+                Path(str(markata.config.get("output_dir", "markout")))
+                / "_logs"
+                / (logging.getLevelName(level).lower() + ".log"),
+            )
+        )
+    )
+    if has_file_handler(log_file):
+        return log_file
+
+    if not log_file.parent.exists():
+        log_file.parent.mkdir(parents=True)
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(level)
+    fh_formatter = logging.Formatter(
+        "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+    )
+    fh.setFormatter(fh_formatter)
+    logging.getLogger("").addHandler(fh)
+
+    return log_file
+
+
+def setup_html_log(markata: "Markata", level: int = logging.INFO) -> Path:
+    """
+    sets up an html log in markata's configured `log_dir`, or
+    `output_dir/_logs` if `log_dir` is not configured.  The log file will be
+    named after the `<levelname>/index.html`.  The goal of this is to give
+    """
 
     log_file = Path(
         str(
@@ -26,17 +140,13 @@ def setup_log(markata: "Markata", level: int = logging.INFO) -> Path:
                 "log_dir",
                 Path(str(markata.config.get("output_dir", "markout")))
                 / "_logs"
-                / logging.getLevelName(level)
+                / logging.getLevelName(level).lower()
                 / "index.html",
             )
         )
     )
 
-    logger = logging.getLogger()
-    files = [
-        l.baseFilename for l in logger.handlers if isinstance(l, logging.FileHandler)
-    ]
-    if str(log_file.absolute()) in files:
+    if has_file_handler(log_file):
         return log_file
 
     if not log_file.parent.exists():
@@ -80,20 +190,31 @@ def setup_log(markata: "Markata", level: int = logging.INFO) -> Path:
 @hook_impl(tryfirst=True)
 def configure(markata: "Markata") -> None:
 
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
-        datefmt="%m-%d %H:%M",
-        filename="debug.log",
-        filemode="w",
-    )
+    # log_file = Path(
+    #     str(
+    #         markata.config.get(
+    #             "log_dir",
+    #             Path(str(markata.config.get("output_dir", "markout")))
+    #             / "_logs"
+    #             / "debug.log",
+    #         )
+    #     )
+    # )
+
+    # logging.basicConfig(
+    #     level=logging.DEBUG,
+    #     format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+    #     datefmt="%m-%d %H:%M",
+    #     filemode="w",
+    # )
 
     setup_log(markata, logging.DEBUG)
     setup_log(markata, logging.INFO)
     setup_log(markata, logging.WARNING)
 
-    console = RichHandler(rich_tracebacks=True)
-    console.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(message)s")
-    console.setFormatter(formatter)
-    logging.getLogger("").addHandler(console)
+    if not has_rich_handler():
+        console = RichHandler(rich_tracebacks=True)
+        console.setLevel(logging.INFO)
+        formatter = logging.Formatter("%(message)s")
+        console.setFormatter(formatter)
+        logging.getLogger("").addHandler(console)
