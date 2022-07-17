8ea42e10c5e229da782e1f0cf3358955de4e4e03
autobump <autobump@markata.dev>
2022-07-17 23:43:43 +0000

    Bump version: 0.3.0.b1 → 0.3.0.b2

--- markata/__init__.py
+++ markata/__init__.py
@@ -29,7 +29,7 @@ from markata.cli.server import Server
 from markata.cli.summary import Summary
 from markata.lifecycle import LifeCycle
 
-__version__ = "0.3.0.b1"
+__version__ = "0.3.0.b2"
 
 
 logger = logging.getLogger("markata")
