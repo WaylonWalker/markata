01e0df38ffc1d3aa44d6f878112870d469e23c53
autobump <autobump@markata.dev>
2022-08-06 15:30:48 +0000

    Bump version: 0.3.0.b4 → 0.3.0.b5

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ README = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
 
 setup(
     name=NAME,
-    version="0.3.0.b4",
+    version="0.3.0.b5",
     url="https://markata.dev",
     author="Waylon Walker",
     author_email="waylon@waylonwalker.com",
