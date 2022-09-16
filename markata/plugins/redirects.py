5e1935732247aaa1f61610d720bc58f116e80e08
Waylon S. Walker <waylon@waylonwalker.com>
2022-09-16 08:59:42 -0500

    wip docs

--- markata/plugins/redirects.py
+++ markata/plugins/redirects.py
@@ -1,3 +1,25 @@
+"""
+Creates redirects for times when your backend server can't.
+
+## Configuration
+
+Enable the redirect hook by adding it to your list of hooks.
+
+``` toml
+[markata]
+
+assets_dir = "static" # Were you keep static assets to copy into the project, default is static
+hooks = [
+   "markata.plugins.redirects", # creates redirects from static/_redirects file
+   "markata.plugins.copy_assets", # copies your static assets into the output_dir (default: `markout`)
+  ...
+]
+```
+
+
+
+
+"""
 from dataclasses import asdict, dataclass
 from pathlib import Path
 from typing import TYPE_CHECKING
@@ -20,19 +42,26 @@ class Redirect:
 
 @hook_impl
 def save(markata: "Markata") -> None:
+    """
+    saves an index.html in the directory called out by the redirect.
+    """
     assets_dir: str = str(markata.config.get("assets_dir", "static"))
     redirects_file = Path(
         str(markata.config.get("redirects", Path(assets_dir) / "_redirects"))
     )
+    if redirects_file.exists():
+        raw_redirects = redirects_file.read_text().split("\n")
+    else:
+        raw_redirects = []
 
     redirects = [
         Redirect(*s)
-        for r in redirects_file.read_text().split("\n")
+        for r in 
         if "*" not in r and len(s := r.split()) == 2 and not r.strip().startswith("#")
     ]
 
     if "redirect_template" in markata.config:
-        template_file = Path(markata.config.get("redirect_template"))
+        template_file = Path(str(markata.config.get("redirect_template")))
     else:
         template_file = DEFAULT_REDIRECT_TEMPLATE
     template = Template(template_file.read_text())
