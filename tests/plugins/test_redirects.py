319bfe6b127b1af693145a1be2e470e60c0528c5
Waylon S. Walker <waylon@waylonwalker.com>
2022-09-18 22:46:59 -0500

    test redirects

--- tests/plugins/test_redirects.py
+++ tests/plugins/test_redirects.py
@@ -1,33 +1,13 @@
 """
 Tests the redirects plugin
 """
-import os
-from contextlib import contextmanager
 from pathlib import Path
 
 import pytest
+from conftest import set_directory
 
 from markata import Markata
 from markata.plugins import redirects
-
-
-@contextmanager
-def set_directory(path: str) -> None:
-    """Sets the cwd within the context
-
-    Args:
-        path (Path): The path to the cwd
-
-    Yields:
-        None
-    """
-
-    origin = Path().absolute()
-    try:
-        os.chdir(path)
-        yield
-    finally:
-        os.chdir(origin)
 
 
 @pytest.mark.parametrize(
