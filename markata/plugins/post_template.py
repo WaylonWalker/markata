a5312ff5f914a1a8e5a4dd1360e6cdb4caa42e50
Waylon S. Walker <waylon@waylonwalker.com>
2022-09-20 21:03:06 -0500

    wip

--- markata/plugins/post_template.py
+++ markata/plugins/post_template.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 
 from jinja2 import Template, Undefined
+from more_itertools import flatten
 
 from markata.hookspec import hook_impl
 
@@ -12,6 +13,51 @@ if TYPE_CHECKING:
 class SilentUndefined(Undefined):
     def _fail_with_undefined_error(self, *args, **kwargs):
         return ""
+
+
+@hook_impl
+def configure(markata: "Markata") -> None:
+    '''
+    massages the configuration limitations of toml to allow a little bit easier
+    experience to the end user making configurations while allowing an simpler
+    jinja template.
+
+    Adding this snippet to your post template will allow users to use the same
+    configuration as the default template.
+
+    ``` html
+    {{ config.get('head', {}).pop('text') if 'text' in config.get('head',{}).keys() }}{% for tag, meta in config.get('head', {}).items() %}{% for _meta in meta %}
+    <{{ tag }} {% for attr, value in _meta.items() %}{{ attr }}="{{ value }}"{% endfor %}/> {% endfor %}{% endfor %}
+    ```
+
+    Here is an example config
+
+    ``` toml
+    [[markata.head.text]]
+    value = """
+    some script
+    """
+
+    [[markata.head.text]]
+    value="""
+    some style
+    """
+
+    [[markata.head.meta]]
+    name = "og:type"
+    property = "og:type"
+    content = "article"
+
+    [[markata.head.meta]]
+    name = "og:author"
+    property = "og:author"
+    content = "Waylon Walker"
+    ```
+    '''
+
+    markata.config["head"]["text"] = "\n".join(
+        flatten([t.values() for t in markata.config["head"]["text"]])
+    )
 
 
 @hook_impl
