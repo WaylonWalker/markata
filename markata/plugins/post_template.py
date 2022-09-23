f9e79ee436d9a2472e38e52c2460768a63dc2069
Waylon Walker <waylon@waylonwalker.com>
2022-09-23 08:31:56 -0500

    Configurable post head (#85)
    * wip
    
    * take care of string list types
    
    * wip tests
    
    * docs
    
    * add toml to known_third_party
    
    * docs

--- markata/plugins/post_template.py
+++ markata/plugins/post_template.py
@@ -1,7 +1,63 @@
+"""
+
+
+## Add head configuration
+
+This snippet allows users to configure their head in `markata.toml`.
+
+``` html
+{{ config.get('head', {}).pop('text') if 'text' in config.get('head',{}).keys() }}{% for tag, meta in config.get('head', {}).items() %}{% for _meta in meta %}
+<{{ tag }} {% for attr, value in _meta.items() %}{{ attr }}="{{ value }}"{% endfor %}/> {% endfor %}{% endfor %}
+```
+
+Users can specify any sort of tag in their `markata.toml`
+
+```
+[[markata.head.meta]]
+name = "og:type"
+content = "article"
+
+[[markata.head.meta]]
+name = "og:author"
+content = "Waylon Walker"
+```
+
+The above configuration becomes this once rendered.
+
+``` html
+<meta name='og:type' content='article' />
+<meta name='og:Author' content='Waylon Walker' />
+```
+
+Optionally users can also specify plain text to be appended to the head of
+their documents.  This works well for things that involve full blocks.
+
+``` toml
+[[markata.head.text]]
+value = '''
+<script>
+    console.log('hello world')
+</script>
+'''
+
+[[markata.head.text]]
+value='''
+html  {
+    font-family: "Space Mono", monospace;
+    background: var(--color-bg);
+    color: var(--color-text);
+}
+'''
+```
+
+
+
+"""
 from pathlib import Path
 from typing import TYPE_CHECKING
 
 from jinja2 import Template, Undefined
+from more_itertools import flatten
 
 from markata.hookspec import hook_impl
 
@@ -12,6 +68,23 @@ if TYPE_CHECKING:
 class SilentUndefined(Undefined):
     def _fail_with_undefined_error(self, *args, **kwargs):
         return ""
+
+
+@hook_impl
+def configure(markata: "Markata") -> None:
+    """
+    Massages the configuration limitations of toml to allow a little bit easier
+    experience to the end user making configurations while allowing an simpler
+    jinja template.  This enablees the use of the `markata.head.text` list in
+    configuration.
+    """
+
+    raw_text = markata.config.get("head", {}).get("text", "")
+
+    if isinstance(raw_text, list):
+        markata.config["head"]["text"] = "\n".join(
+            flatten([t.values() for t in raw_text])
+        )
 
 
 @hook_impl
