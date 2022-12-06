f5dcc85245a50fd25cda7df33d50498c60c65eb3
Waylon S. Walker <waylon@waylonwalker.com>
2022-12-05 20:32:17 -0600

    use tmp_path, not factory

--- tests/test_one_default_page.py
+++ tests/test_one_default_page.py
@@ -7,9 +7,11 @@ import pytest
 from markata import Markata
 
 
-@pytest.fixture(scope="session")
-def make_index(tmp_path_factory: Any) -> Any:
-    fn = tmp_path_factory.mktemp("pages") / "index.md"
+@pytest.fixture()
+def make_index(tmp_path: Any) -> Any:
+    pages = tmp_path / "pages"
+    pages.mkdir()
+    fn = pages / "index.md"
     fn.write_text(
         textwrap.dedent(
             """
@@ -26,35 +28,35 @@ def make_index(tmp_path_factory: Any) -> Any:
             """
         )
     )
-    return tmp_path_factory
+    return tmp_path
 
 
 def test_loaded(make_index: Any) -> None:
-    os.chdir(make_index.getbasetemp())
+    os.chdir(make_index)
     m = Markata()
     assert len(m.articles) == 1
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def test_run(make_index: Any) -> Any:
-    os.chdir(make_index.getbasetemp())
+    os.chdir(make_index)
     m = Markata()
     m.run()
     return make_index
 
 
 def test_markout_exists(test_run: Any) -> Any:
-    markout = test_run.getbasetemp() / "markout"
+    markout = test_run / "markout"
     assert markout.exists()
 
 
 def test_index_exists(test_run: Any) -> Any:
-    markout = test_run.getbasetemp() / "markout"
+    markout = test_run / "markout"
     index = markout / "index.html"
     assert index.exists()
 
 
 def test_rss_exists(test_run: Any) -> Any:
-    markout = test_run.getbasetemp() / "markout"
+    markout = test_run / "markout"
     rss = markout / "rss.xml"
     assert rss.exists()
