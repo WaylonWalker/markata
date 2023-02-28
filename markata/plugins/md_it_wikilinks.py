05e88ac9e055cfb1ba8f0587c11dcbf2f00c862c
Waylon S. Walker <waylon@waylonwalker.com>
2023-02-28 15:03:02 -0600

    docs

--- markata/plugins/md_it_wikilinks.py
+++ markata/plugins/md_it_wikilinks.py
@@ -1,3 +1,46 @@
+"""
+Wikilinks depicted with `\[[wikilinks]]` can be enabled for sites using the
+`markdown-it-py` plugin markata will look through all posts matching up the
+file stem to the wiki link and inserting the slug as the href.
+
+???+ note normal wikilink
+
+    Here is a link to a markdown file `docs/nav.md`, and the url becomes
+    [/nav](/nav).
+
+    ```md
+    [[nav]]
+    ```
+
+    When rendered out this wikilink will become an anchor link.
+
+    ```html
+    <a class="wikilink" href="/nav">load</a>
+    ```
+
+    > This behaves just like the standard wikilink
+
+A special feature that markata brings is slug lookup.  It is able to not only
+blindly link to the route specified, but will look up the slug of an article.
+
+
+???+ note markata slug lookup
+    Markata has a load plugin that is generated with the [[docs]] plugin.  It's
+    filepath is `markata/plugins/load.py`, so it can be referenced by the file
+    stem `load`.
+
+    ```md
+    [[load]]
+    ```
+
+    Markata will look up the article by the file stem, grab the first article,
+    and use its slug as the href.  This turns it into an anchor link that looks
+    like this.
+
+    ```html
+    <a class="wikilink" href="/markata/plugins/load">load</a>
+    ```
+"""
 import logging
 
 from markdown_it import MarkdownIt
@@ -14,9 +57,16 @@ def wikilinks_plugin(
 ):
     """A plugin to create wikilinks tokens.
     These, token should be handled by the renderer.
-    Example::
 
-        a  b
+    ???+ example
+
+        ```md title=markdown
+        [[nav]]
+        ```
+
+        ```html title=html
+        <a class="wikilink" href="/nav">load</a>
+        ```
     """
 
     start_char = ord(start_delimiter)
@@ -59,22 +109,28 @@ def wikilinks_plugin(
         token = state.push("link_open", "a", 1)
         token.block = False
         token.attrSet("class", "wikilink")
-        link = text
+        if "#" in text:
+            link, id = text.split("#")
+        else:
+            link, id = text, None
         possible_pages = markata.filter(
-            f'path.split("/")[-1].split(".")[0] == "{text}"'
+            f'path.split("/")[-1].split(".")[0].replace("_", "-") == "{link.replace("_", "-")}"'
         )
         if len(possible_pages) == 1:
             link = possible_pages[0].get("slug", f"/{text}")
         elif len(possible_pages) > 1:
             logger.warning(
-                f"wikilink [[{text}]] has duplicate matches, defaulting to the first"
+                f"wikilink [[{text}]] ({link}, {id}) has duplicate matches, defaulting to the first"
             )
             link = possible_pages[0].get("slug", f"/{text}")
         else:
             logger.warning(
-                f"wikilink [[{text}]] has no matches, defaulting to '/{text}'"
+                f"wikilink [[{text}]] ({link}, {id}) no matches, defaulting to '/{text}'"
             )
             link = text
+
+        if id and not link.endswith(f"#{id}"):
+            link = f"{link}#{id}"
 
         token.attrSet("href", f"/{link}")
         content_token = state.push("text", "", 0)
