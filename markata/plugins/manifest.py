0f1ee227717aca3a2800650a8f9c4de4296e30a8
Waylon S. Walker <waylon@waylonwalker.com>
2023-12-26 11:33:38 -0600

    link manifest in templates not here, it's too slow

--- markata/plugins/manifest.py
+++ markata/plugins/manifest.py
@@ -28,27 +28,3 @@ def render(markata: "MarkataIcons") -> None:
     filepath.touch(exist_ok=True)
     with open(filepath, "w+") as f:
         json.dump(manifest, f, ensure_ascii=True, indent=4)
-    config = markata.get_plugin_config(__file__)
-    should_prettify = markata.config.get("prettify_html", False)
-    with markata.cache as cache:
-        for article in markata.iter_articles("add manifest link"):
-            key = markata.make_hash(
-                "seo",
-                "manifest",
-                article.content,
-                article.html,
-            )
-            html_from_cache = markata.precache.get(key)
-
-            if html_from_cache is None:
-                soup = BeautifulSoup(article.html, features="lxml")
-                link = soup.new_tag("link")
-                link.attrs["rel"] = "manifest"
-                link.attrs["href"] = "/manifest.json"
-                soup.head.append(link)
-
-                html = soup.prettify() if should_prettify else str(soup)
-                cache.add(key, html, expire=config["cache_expire"])
-            else:
-                html = html_from_cache
-            article.html = html
