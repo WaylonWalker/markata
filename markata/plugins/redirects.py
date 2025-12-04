ab34dc3a42dc9b22fddaf2480a6d2ec9ac64140e
Waylon S. Walker <waylon@waylonwalker.com>
2025-12-04 12:53:39 -0600

    fix: add cache invalidation for redirects and jinja_md templates
    - redirects.py: Include template mtime in cache key to detect template changes
    - jinja_md.py: Include post metadata and markata version in cache key
    
    Previously:
    - Changing redirect template wouldn't regenerate redirect files
    - Changing post metadata in jinja templates wouldn't invalidate cache
    - Changes to markata context wouldn't trigger re-render
    
    Now cache properly invalidates when:
    - Redirect template file is modified
    - Post metadata changes that affects jinja rendering
    - Markata version changes (API changes)

--- markata/plugins/redirects.py
+++ markata/plugins/redirects.py
@@ -160,14 +160,6 @@ def save(markata: "Markata") -> None:
     else:
         raw_redirects = []
 
-    key = markata.make_hash("redirects", "raw_redirects", raw_redirects)
-    with markata.cache as cache:
-        cache.get(key)
-        if cache.get(key) == "done":
-            return
-
-        cache.set(key, "done", expire=markata.config.default_cache_expire)
-
     redirects = [
         Redirect(original=s[0], new=s[1], markata=markata)
         for r in raw_redirects
@@ -178,6 +170,18 @@ def save(markata: "Markata") -> None:
         template_file = Path(str(markata.config.get("redirect_template")))
     else:
         template_file = DEFAULT_REDIRECT_TEMPLATE
+    
+    # Get template mtime to bust cache when template changes
+    template_mtime = template_file.stat().st_mtime if template_file.exists() else 0
+    
+    key = markata.make_hash("redirects", "raw_redirects", raw_redirects, str(template_mtime))
+    with markata.cache as cache:
+        cache.get(key)
+        if cache.get(key) == "done":
+            return
+
+        cache.set(key, "done", expire=markata.config.default_cache_expire)
+
     template = Template(template_file.read_text())
 
     for redirect in redirects:
